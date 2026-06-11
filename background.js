6180ff41738b2e2c0e0c37b3e4a7846faada73dd
Evan Moreshead <evan.moreshead@dutchie.com>
2026-06-11 18:04:01 -0400

    Scope side panel to the tab it was opened on

--- background.js
+++ background.js
@@ -1,3 +1,8 @@
-chrome.action.onClicked.addListener((tab) => {
+chrome.action.onClicked.addListener(async (tab) => {
+  await chrome.sidePanel.setOptions({
+    tabId: tab.id,
+    path: 'sidepanel.html',
+    enabled: true,
+  });
   chrome.sidePanel.open({ tabId: tab.id });
 });
